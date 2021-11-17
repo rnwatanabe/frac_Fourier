dfe556952a3b41930e957312e1ad865e1d485bfe
Renato Naville Watanabe <renato.navillewatanabe@gmail.com>
2021-11-17 08:46:42 -0300

    memory optimization

--- frac_Fourier.py
+++ frac_Fourier.py
@@ -47,6 +47,7 @@ def fractional_Fourier_transform(x, a, t, dt0, a0, N=0):
     print(ta.shape)
     print(xa.shape)
     T = Aphi/(2*tfmax)*torch.exp(1j*math.pi*(alpha-beta)*ta.T**2)*torch.exp(1j*math.pi*beta*(ta.T-ta)**2)*torch.exp(1j*math.pi*(alpha-beta)*ta**2)
+    print('T.shape')
     xa1 = T@x
     print('10')
     for i in range(len(ta)):      
